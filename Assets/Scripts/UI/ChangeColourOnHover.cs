8b1dcb2fb7e7ac5dc40bbf62fece504492e2fb70
Kaladania <72551649+Kaladania@users.noreply.github.com>
2025-03-14 01:13:39 +0000

    UI Element Changes on Hover
    UI Menu slice changes color when hovered.

--- Assets/Scripts/UI/ChangeColourOnHover.cs
+++ Assets/Scripts/UI/ChangeColourOnHover.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
 
-public class ChangeColourOnHover : MonoBehaviour
+
+public class ChangeColourOnHover : MonoBehaviour, IPointerEnterHandler
 {
+    [System.Serializable]
+    public struct MenuSlice
+    {
+        public UnityEngine.UI.Image image;
+        public Color color;
+    }
+
+    [SerializeField]
+    private List<MenuSlice> _menuSlices;
+
+    [SerializeField] private Color _hoverColor = Color.red;
+
+    private int _UILayer = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _UILayer = LayerMask.NameToLayer("UI");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _menuSlices[0].image.color = _hoverColor;
+        //this.gameObject.GetComponentInParent<Image>().color = _hoverColor;
+        Debug.Log("The cursor entered the selectable UI element.");
     }
 }
